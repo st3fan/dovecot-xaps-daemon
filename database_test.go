eec25158ba0e68031d1736943243d14e842fd040
frederik <frederik.schwan@linux.com>
2018-01-01 22:14:47 +0100

    update copyright information

--- database_test.go
+++ database_test.go
@@ -2,6 +2,7 @@
 // The MIT License (MIT)
 //
 // Copyright (c) 2015 Stefan Arentz <stefan@arentz.ca>
+// Copyright (c) 2017 Frederik Schwan <frederik dot schwan at linux dot com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
